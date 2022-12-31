2d1a76f3d9b8b5e4622a589166040b777311e3e8
Arthur Luan <arthurluancouto214@gmail.com>
2022-12-31 15:05:55 +0000

    feat: config mui react

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import React from "react";
 import { Router } from "./routes/router";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
 function App() {
-  return <Router />;
+  const theme = createTheme();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router />
+    </ThemeProvider>
+  );
 }
 
 export default App;
