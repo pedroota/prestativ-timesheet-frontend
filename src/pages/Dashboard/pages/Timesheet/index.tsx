45c54910acd489434065d4895ee46e89568ea670
Filipe Bacof01~ <filipebacof@gmail.com>
2023-01-10 17:37:34 -0300

    Tela Timesheet inicializada

--- src/pages/Dashboard/pages/Timesheet/index.tsx
+++ src/pages/Dashboard/pages/Timesheet/index.tsx
@@ -1,3 +1,5 @@
+import { useQuery } from "@tanstack/react-query";
+import { deleteHours, getHours } from "services/hours.service";
 import { styled } from "@mui/material/styles";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -5,6 +7,10 @@ import TableCell, { tableCellClasses } from "@mui/material/TableCell";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import { Hours } from "interfaces/hours.interface";
+import { Button, Checkbox } from "@mui/material";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -26,66 +32,145 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   },
 }));
 
-const rows = [
-  {
-    id: 1,
-    date: "09/12",
-    day: "SEX",
-    startDate: "15:00",
-    endDate: "17:00",
-    hours: "2h",
-    client: "Multilaser",
-    project: "Projeto XYZ",
-    activity: "Atividade Barbada que demorei demais e virou bola de neve",
-    approved: "Em andamento",
-  },
-  {
-    id: 2,
-    date: "09/12",
-    day: "SEX",
-    startDate: "15:00",
-    endDate: "17:00",
-    hours: "2h",
-    client: "Multilaser",
-    project: "Projeto XYZ",
-    activity: "Atividade Barbada que demorei demais e virou bola de neve",
-    approved: "Em andamento",
-  },
-];
-
 export function Timesheet() {
+  const { data: hours } = useQuery(["hours"], () => getHours());
+
   return (
-    <Paper className="c-timesheet">
-      <Table aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell align="center">Data</StyledTableCell>
-            <StyledTableCell align="center">Dia</StyledTableCell>
-            <StyledTableCell align="center">Início</StyledTableCell>
-            <StyledTableCell align="center">Fim</StyledTableCell>
-            <StyledTableCell align="center">Horas</StyledTableCell>
-            <StyledTableCell align="center">Cliente</StyledTableCell>
-            <StyledTableCell align="center">Projeto</StyledTableCell>
-            <StyledTableCell align="center">Atividade</StyledTableCell>
-            <StyledTableCell align="center">Status</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <StyledTableRow key={row.id}>
-              <StyledTableCell align="center">{row.date}</StyledTableCell>
-              <StyledTableCell align="center">{row.day}</StyledTableCell>
-              <StyledTableCell align="center">{row.startDate}</StyledTableCell>
-              <StyledTableCell align="center">{row.endDate}</StyledTableCell>
-              <StyledTableCell align="center">{row.hours}</StyledTableCell>
-              <StyledTableCell align="center">{row.client}</StyledTableCell>
-              <StyledTableCell align="center">{row.project}</StyledTableCell>
-              <StyledTableCell align="center">{row.activity}</StyledTableCell>
-              <StyledTableCell align="center">{row.approved}</StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </Paper>
+    <div>
+      <Paper className="c-timesheet">
+        <Table aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell align="center">
+                Horário
+                <br />
+                Inicial
+              </StyledTableCell>
+              <StyledTableCell align="center">
+                Horário
+                <br />
+                Final
+              </StyledTableCell>
+              <StyledTableCell align="center">Total</StyledTableCell>
+              <StyledTableCell align="center">Ajuste</StyledTableCell>
+              <StyledTableCell align="center">
+                Total
+                <br />
+                c/ Ajuste
+              </StyledTableCell>
+              <StyledTableCell align="center">Cliente</StyledTableCell>
+              <StyledTableCell align="center">Projeto</StyledTableCell>
+              <StyledTableCell align="center">Atividade</StyledTableCell>
+              <StyledTableCell align="center">Consultor</StyledTableCell>
+              <StyledTableCell align="center">
+                Escopo
+                <br />
+                Fechado
+              </StyledTableCell>
+              <StyledTableCell align="center">Faturável</StyledTableCell>
+              <StyledTableCell align="center">Lançado</StyledTableCell>
+              <StyledTableCell align="center">Aprovado</StyledTableCell>
+              <StyledTableCell align="center">
+                Número do
+                <br />
+                Chamado
+              </StyledTableCell>
+              <StyledTableCell align="center">Data Sistema</StyledTableCell>
+              <StyledTableCell align="center">Data Edição</StyledTableCell>
+              <StyledTableCell align="center">Controles</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {hours?.data.map(
+              ({
+                _id,
+                initial,
+                final,
+                adjustment,
+                relClient,
+                relProject,
+                relActivity,
+                relUser,
+                closedScope,
+                billable,
+                released,
+                approved,
+                callNumber,
+                createdAt,
+                updatedAt,
+              }: Hours) => (
+                <StyledTableRow key={_id}>
+                  <StyledTableCell align="center">
+                    {new Date(initial).toLocaleDateString()}
+                    <br />
+                    {new Date(initial).getHours() +
+                      ":" +
+                      new Date(initial).getMinutes()}
+                  </StyledTableCell>
+                  <StyledTableCell align="center">
+                    {new Date(final).toLocaleDateString()}
+                    <br />
+                    {new Date(final).getHours() +
+                      ":" +
+                      new Date(final).getMinutes()}
+                  </StyledTableCell>
+                  <StyledTableCell align="center">total</StyledTableCell>
+                  <StyledTableCell align="center">
+                    {adjustment / 60 / 60 / 1000}
+                  </StyledTableCell>
+                  <StyledTableCell align="center">total ajuste</StyledTableCell>
+                  <StyledTableCell align="center">{relClient}</StyledTableCell>
+                  <StyledTableCell align="center">{relProject}</StyledTableCell>
+                  <StyledTableCell align="center">
+                    {relActivity}
+                  </StyledTableCell>
+                  <StyledTableCell align="center">{relUser}</StyledTableCell>
+                  <StyledTableCell align="center">
+                    {!closedScope ? <Checkbox /> : <Checkbox defaultChecked />}
+                    {closedScope ? "sim" : "Não"}
+                  </StyledTableCell>
+                  <StyledTableCell align="center">
+                    {!billable ? <Checkbox /> : <Checkbox defaultChecked />}
+                    {billable ? "sim" : "Não"}
+                  </StyledTableCell>
+                  <StyledTableCell align="center">
+                    {!released ? <Checkbox /> : <Checkbox defaultChecked />}
+                    {released ? "sim" : "Não"}
+                  </StyledTableCell>
+                  <StyledTableCell align="center">
+                    {!approved ? <Checkbox /> : <Checkbox defaultChecked />}
+                    {approved ? "sim" : "Não"}
+                  </StyledTableCell>
+                  <StyledTableCell align="center">{callNumber}</StyledTableCell>
+                  <StyledTableCell align="center">
+                    {new Date(createdAt).toLocaleDateString()}
+                    <br />
+                    {new Date(createdAt).getHours() +
+                      ":" +
+                      new Date(createdAt).getMinutes()}
+                  </StyledTableCell>
+                  <StyledTableCell align="center">
+                    {new Date(updatedAt).toLocaleDateString()}
+                    <br />
+                    {new Date(updatedAt).getHours() +
+                      ":" +
+                      new Date(updatedAt).getMinutes()}
+                  </StyledTableCell>
+                  <StyledTableCell align="center">
+                    <EditIcon />
+                    <DeleteIcon
+                      onClick={() => {
+                        deleteHours(_id);
+                      }}
+                    />
+                  </StyledTableCell>
+                </StyledTableRow>
+              )
+            )}
+          </TableBody>
+        </Table>
+        <Button>Novo Lançamento</Button>
+      </Paper>
+    </div>
   );
 }
---
+++ src/services/hours.service.ts
@@ -0,0 +1,38 @@
+import { Api } from "./api.service";
+import { Hours } from "interfaces/hours.interface";
+
+export const getHours = async () => {
+  const result = await Api.get("/hours");
+
+  return result;
+};
+
+export const createHours = async ({
+  initial,
+  final,
+  adjustment,
+  relClient,
+  relProject,
+  relActivity,
+  relUser,
+  callNumber,
+}: Hours) => {
+  const result = await Api.post("/hours", {
+    initial,
+    final,
+    adjustment,
+    relClient,
+    relProject,
+    relActivity,
+    relUser,
+    callNumber,
+  });
+
+  return result;
+};
+
+export const deleteHours = async (_id: string) => {
+  const result = await Api.delete(`/hours/${_id}`);
+
+  return result;
+};
