05384d7c21048750ef8dc2de405c1dba450964ec
Filipe Bacof01~ <filipebacof@gmail.com>
2023-02-09 13:41:53 -0300

    ajustada validade padrão

--- src/pages/Dashboard/pages/RegisterActivity/index.tsx
+++ src/pages/Dashboard/pages/RegisterActivity/index.tsx
@@ -127,7 +127,7 @@ export function RegisterActivity() {
     date.setMonth(date.getMonth() + 1);
     const year = date.getFullYear();
     const month = date.getMonth() + 1;
-    const day = date.getDay();
+    const day = date.getDate();
     return `${year}-${month < 10 ? `0${month}` : month}-${
       day < 10 ? `0${day}` : day
     }`;
