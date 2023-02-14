f4628897370c68a544587c895d4eb5a811b9d50d
Pedro Otavio <pedrocoutosoares214@gmail.com>
2023-02-14 01:54:29 +0000

    fix: interceptor bug api

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -5,7 +5,15 @@ const { REACT_APP_API_URL } = process.env;
 
 export const Api: AxiosInstance = Axios.create({
   baseURL: `${REACT_APP_API_URL}`,
-  headers: {
-    Authorization: `Bearer ${Cookies.get("token")}`,
-  },
 });
+
+Api.interceptors.request.use(
+  (config) => {
+    config.headers.Authorization = `Bearer ${Cookies.get("token")}`;
+
+    return Promise.resolve(config);
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
