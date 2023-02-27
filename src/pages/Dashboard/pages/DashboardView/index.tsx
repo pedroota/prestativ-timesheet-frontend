83829b2789edeb22a2b1da8ef4bc7f4650369c1d
Filipe Bacof01~ <filipebacof@gmail.com>
2023-02-27 14:56:57 -0300

    dashb correct w isLoadin'

--- src/pages/Dashboard/pages/DashboardView/index.tsx
+++ src/pages/Dashboard/pages/DashboardView/index.tsx
@@ -1,14 +1,21 @@
-// import { Table, TableHead, TableRow, TableBody } from "@mui/material";
+import {
+  Table,
+  TableHead,
+  TableRow,
+  TableBody,
+  Box,
+  CircularProgress,
+} from "@mui/material";
 import { useQuery } from "@tanstack/react-query";
 import { Permission } from "components/Permission";
-// import { StyledTableCell } from "components/StyledTableCell";
-// import { StyledTableRow } from "components/StyledTableRow";
-// import { Hours } from "interfaces/hours.interface";
+import { StyledTableCell } from "components/StyledTableCell";
+import { StyledTableRow } from "components/StyledTableRow";
+import { Hours } from "interfaces/hours.interface";
 import { getHoursLatest } from "services/hours.service";
 import { generateTotalHours } from "utils/timeControl";
 
 export function DashboardView() {
-  const { data: hoursThisMonth } = useQuery(
+  const { data: hoursThisMonth, isLoading } = useQuery(
     ["hours"],
     async () => await getHoursLatest()
   );
@@ -26,121 +33,141 @@ export function DashboardView() {
   );
   console.log(updatedHoursThisMonth);
   // cria um novo array com objetos formatados = clientes -> projetos dele -> atividades dele -> soma de todos os lançamentos de horas
-  // const grouped = updatedHoursThisMonth.reduce(
-  //   (
-  //     acc: {
-  //       [x: string]: {
-  //         clientName: string;
-  //         projects: {
-  //           [x: string]: {
-  //             projectName: string;
-  //             activities: {
-  //               [x: string]: { totalHours: number };
-  //             };
-  //           };
-  //         };
-  //       };
-  //     },
-  //     curr: Hours & { totalHours: string }
-  //   ) => {
-  //     const { relClient, relProject, relActivity, totalHours } = curr;
-  //     const clientId = relClient._id;
-  //     const projectId = relProject._id;
-  //     const activityTitle = relActivity.title;
+  const grouped =
+    updatedHoursThisMonth &&
+    updatedHoursThisMonth.reduce(
+      (
+        acc: {
+          [x: string]: {
+            clientName: string;
+            projects: {
+              [x: string]: {
+                projectName: string;
+                activities: {
+                  [x: string]: { totalHours: number };
+                };
+              };
+            };
+          };
+        },
+        curr: Hours & { totalHours: string }
+      ) => {
+        const { relClient, relProject, relActivity, totalHours } = curr;
+        const clientId = relClient._id;
+        const projectId = relProject._id;
+        const activityTitle = relActivity.title;
 
-  //     if (!acc[clientId]) {
-  //       acc[clientId] = {
-  //         clientName: relClient.name,
-  //         projects: {},
-  //       };
-  //     }
+        if (!acc[clientId]) {
+          acc[clientId] = {
+            clientName: relClient.name,
+            projects: {},
+          };
+        }
 
-  //     if (!acc[clientId].projects[projectId]) {
-  //       acc[clientId].projects[projectId] = {
-  //         projectName: relProject.title,
-  //         activities: {},
-  //       };
-  //     }
+        if (!acc[clientId].projects[projectId]) {
+          acc[clientId].projects[projectId] = {
+            projectName: relProject.title,
+            activities: {},
+          };
+        }
 
-  //     if (!acc[clientId].projects[projectId].activities[activityTitle]) {
-  //       acc[clientId].projects[projectId].activities[activityTitle] = {
-  //         totalHours: 0,
-  //       };
-  //     }
+        if (!acc[clientId].projects[projectId].activities[activityTitle]) {
+          acc[clientId].projects[projectId].activities[activityTitle] = {
+            totalHours: 0,
+          };
+        }
 
-  //     acc[clientId].projects[projectId].activities[activityTitle].totalHours +=
-  //       parseFloat(totalHours);
+        acc[clientId].projects[projectId].activities[
+          activityTitle
+        ].totalHours += parseFloat(totalHours);
 
-  //     return acc;
-  //   },
-  //   {}
-  // );
+        return acc;
+      },
+      {}
+    );
 
-  // console.log(grouped);
+  console.log(isLoading && grouped);
 
   return (
     <Permission roles={["DASHBOARD"]}>
       <h1>Dashboard</h1>
-      {/* <Table className="c-table" aria-label="customized table">
-        <TableHead>
-          <TableRow className="c-table--reset-head">
-            <StyledTableCell align="center">Cliente</StyledTableCell>
-            <StyledTableCell align="center">Projeto</StyledTableCell>
-            <StyledTableCell align="center">Atividade</StyledTableCell>
-            <StyledTableCell align="center">Total de Horas</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {Object.keys(grouped).map((clientId) => (
-            <>
-              <StyledTableRow key={clientId}>
-                <StyledTableCell component="th" scope="row" align="center">
-                  {grouped[clientId].clientName}
-                </StyledTableCell>
-                <StyledTableCell />
-                <StyledTableCell />
-                <StyledTableCell />
-              </StyledTableRow>
-              {Object.keys(grouped[clientId].projects).map((projectId) => (
-                <>
-                  <StyledTableRow key={projectId}>
-                    <StyledTableCell />
-                    <StyledTableCell component="th" scope="row" align="center">
-                      {grouped[clientId].projects[projectId].projectName}
-                    </StyledTableCell>
-                    <StyledTableCell />
-                    <StyledTableCell />
-                  </StyledTableRow>
-                  {Object.keys(
-                    grouped[clientId].projects[projectId].activities
-                  ).map((activityTitle) => (
-                    <>
-                      <StyledTableRow key={activityTitle}>
-                        <StyledTableCell />
-                        <StyledTableCell />
-                        <StyledTableCell
-                          component="th"
-                          scope="row"
-                          align="center"
-                        >
-                          {activityTitle}
-                        </StyledTableCell>
-                        <StyledTableCell align="center">
-                          {
-                            grouped[clientId].projects[projectId].activities[
-                              activityTitle
-                            ].totalHours
-                          }
-                        </StyledTableCell>
-                      </StyledTableRow>
-                    </>
-                  ))}
-                </>
-              ))}
-            </>
-          ))}
-        </TableBody>
-      </Table> */}
+      {isLoading ? (
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            marginBlock: "4rem",
+          }}
+        >
+          <CircularProgress color="warning" />
+        </Box>
+      ) : (
+        <Table className="c-table" aria-label="customized table">
+          <TableHead>
+            <TableRow className="c-table--reset-head">
+              <StyledTableCell align="center">Cliente</StyledTableCell>
+              <StyledTableCell align="center">Projeto</StyledTableCell>
+              <StyledTableCell align="center">Atividade</StyledTableCell>
+              <StyledTableCell align="center">Total de Horas</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {Object.keys(grouped).map((clientId) => (
+              <>
+                <StyledTableRow key={clientId}>
+                  <StyledTableCell component="th" scope="row" align="center">
+                    {grouped[clientId].clientName}
+                  </StyledTableCell>
+                  <StyledTableCell />
+                  <StyledTableCell />
+                  <StyledTableCell />
+                </StyledTableRow>
+                {Object.keys(grouped[clientId].projects).map((projectId) => (
+                  <>
+                    <StyledTableRow key={projectId}>
+                      <StyledTableCell />
+                      <StyledTableCell
+                        component="th"
+                        scope="row"
+                        align="center"
+                      >
+                        {grouped[clientId].projects[projectId].projectName}
+                      </StyledTableCell>
+                      <StyledTableCell />
+                      <StyledTableCell />
+                    </StyledTableRow>
+                    {Object.keys(
+                      grouped[clientId].projects[projectId].activities
+                    ).map((activityTitle) => (
+                      <>
+                        <StyledTableRow key={activityTitle}>
+                          <StyledTableCell />
+                          <StyledTableCell />
+                          <StyledTableCell
+                            component="th"
+                            scope="row"
+                            align="center"
+                          >
+                            {activityTitle}
+                          </StyledTableCell>
+                          <StyledTableCell align="center">
+                            {
+                              grouped[clientId].projects[projectId].activities[
+                                activityTitle
+                              ].totalHours
+                            }
+                          </StyledTableCell>
+                        </StyledTableRow>
+                      </>
+                    ))}
+                  </>
+                ))}
+              </>
+            ))}
+          </TableBody>
+        </Table>
+      )}
     </Permission>
   );
 }
